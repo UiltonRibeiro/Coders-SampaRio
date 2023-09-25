eb2b6d913e879e4940a5d09a7f071d3564764278
Uilton <uilton_3001@outlook.com>
2023-09-25 11:34:33 -0300

    Init:[Start] Iniciando o Projeto

---
+++ src/main/java/br/com/blog/demo/BlogDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.blog.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/blog/demo/BlogDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
